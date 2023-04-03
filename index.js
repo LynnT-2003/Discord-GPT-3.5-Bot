e322245c2ba578d76e429d3934952033f71c09c9
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-04-04 00:05:19 +0630

    Enhanced responses by fetching previous messages

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv/config");
 
 const { Client, IntentsBitField } = require("discord.js");
 
+const { Configuration, OpenAIApi } = require("openai");
+
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -14,8 +16,46 @@ client.on("ready", () => {
   console.log("The bot is online");
 });
 
-client.on("messageCreate", (message) => {
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+client.on("messageCreate", async (message) => {
   console.log(message);
+  if (message.author.bot) return;
+  if (message.channel.id !== process.env.CHANNEL_ID) return;
+  if (message.content.startsWith("!")) return;
+
+  let conversationLog = [
+    {
+      role: "system",
+      content: "You are a friendly chatbot",
+    },
+  ];
+
+  await message.channel.sendTyping();
+
+  let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+  prevMessages.reverse();
+
+  prevMessages.forEach((msg) => {
+    if (message.content.startsWith("!")) return;
+    if (msg.author.id !== client.user.id && message.author.bot) return;
+    if (msg.author.id !== message.author.id) return;
+
+    conversationLog.push({
+      role: "user",
+      content: msg.content,
+    });
+  });
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: conversationLog,
+  });
+
+  message.reply(result.data.choices[0].message);
 });
 
 client.login(process.env.TOKEN);
